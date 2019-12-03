bf57a80e8e4f54c3328995e20b489e41dee1bf85
Peter Baier <p.baier@outlook.com>
2019-12-03 09:16:28 +0000

    dropdown menu works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'myProfile';
+export class AppComponent implements OnInit, AfterViewInit {
+    isHamburger = true;
+    dropdown;
+    hamburgerIcon;
+    iam;
+
+    form: FormGroup;
+
+    constructor(private readonly formBuilder: FormBuilder) {
+    }
+
+    ngOnInit(): void {
+        this.form = this.formBuilder.group({
+            name: new FormControl('', [Validators.required]),
+            email: new FormControl('', [Validators.required, Validators.email]),
+            message: new FormControl('', [Validators.required]),
+        });
+    }
+
+    openMenu() {
+        this.dropdown.style.height = '10rem';
+        this.dropdown.style.top = '0';
+        this.dropdown.style.borderRadius = '0 0 0 0';
+        this.hamburgerIcon.style.transform = 'rotateX(90deg)';
+        this.isHamburger = false;
+    }
+
+    closeMenu() {
+        this.dropdown.style.height = '0';
+        this.dropdown.style.top = '0';
+        this.dropdown.style.borderRadius = '0 0 0 100%';
+        this.isHamburger = true;
+        this.hamburgerIcon.style.transform = 'rotateX(0deg)';
+    }
+
+    ngAfterViewInit(): void {
+        this.dropdown = document.getElementById('dropdown');
+        this.hamburgerIcon = document.getElementById('hamburgerIcon');
+        this.iam = document.getElementById('iam');
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatMenuModule, MatIconModule, MatButtonModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
